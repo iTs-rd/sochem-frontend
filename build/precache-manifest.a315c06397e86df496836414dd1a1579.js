fe530066835ed9efecf62966082bd0b712e23e05
rudresh gupta <rudreshgpt7@gmail.com>
2021-08-08 00:55:44 +0530

    minor change

---
+++ build/precache-manifest.a315c06397e86df496836414dd1a1579.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a039920a360d516b34f90fcda78668d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8fe0d138e7ef3ad7a60",
+    "url": "/static/css/2.ae85870d.chunk.css"
+  },
+  {
+    "revision": "44d8bb33ebbfe19e74cc",
+    "url": "/static/css/main.5e642502.chunk.css"
+  },
+  {
+    "revision": "f8fe0d138e7ef3ad7a60",
+    "url": "/static/js/2.1e8dfbe9.chunk.js"
+  },
+  {
+    "revision": "4a30b83ba770536d276f85d56dc318a5",
+    "url": "/static/js/2.1e8dfbe9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44d8bb33ebbfe19e74cc",
+    "url": "/static/js/main.256ab2c9.chunk.js"
+  },
+  {
+    "revision": "dd14837b7cedbbdc56aa",
+    "url": "/static/js/runtime-main.524308bd.js"
+  },
+  {
+    "revision": "20330989617d0cc2cee53d2a221c2174",
+    "url": "/static/media/banner.20330989.jpg"
+  },
+  {
+    "revision": "b69e958fbfcbeb398fba17001b9d52ba",
+    "url": "/static/media/bg.b69e958f.jpg"
+  },
+  {
+    "revision": "f6fc44609b8f79e3131d2bd430cd341c",
+    "url": "/static/media/cloud-books.f6fc4460.png"
+  },
+  {
+    "revision": "c040356000cd7604f7b015016f559c79",
+    "url": "/static/media/cloud-coding.c0403560.png"
+  },
+  {
+    "revision": "a6d0ea3ad81a1d354997c4109299209f",
+    "url": "/static/media/cloud-core.a6d0ea3a.png"
+  },
+  {
+    "revision": "c8b21a4e4380252042015438acaa7434",
+    "url": "/static/media/cloud-gate.c8b21a4e.png"
+  },
+  {
+    "revision": "a1f8720b0e578841fe2253596a184d97",
+    "url": "/static/media/cloud-gre.a1f8720b.png"
+  },
+  {
+    "revision": "8d5ebf751945c5d59e36967326362bcf",
+    "url": "/static/media/cloud-hulm.8d5ebf75.png"
+  },
+  {
+    "revision": "195078a4b27eb8e825dbfc93edd54ce9",
+    "url": "/static/media/cloud-oe.195078a4.png"
+  },
+  {
+    "revision": "1472196bad9876032a9824d55bca56e5",
+    "url": "/static/media/cloud-online.1472196b.png"
+  },
+  {
+    "revision": "110272caf82625aca861cb1b4625c0d5",
+    "url": "/static/media/cta01.110272ca.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "9d1652c627c0e718e48132ae2f266686",
+    "url": "/static/media/image.9d1652c6.jpg"
+  },
+  {
+    "revision": "c453b5262c9d13932ddf6cbebae8c3be",
+    "url": "/static/media/slide-home-1.c453b526.jpg"
+  },
+  {
+    "revision": "b978830c79e43bae2dc8d346aa2b7091",
+    "url": "/static/media/slide-home-10.b978830c.jpg"
+  },
+  {
+    "revision": "2fc99ef10b98c9ebaca27e171b721579",
+    "url": "/static/media/slide-home-11.2fc99ef1.jpg"
+  },
+  {
+    "revision": "ac45694e9c85895a5850c32a1b676b31",
+    "url": "/static/media/slide-home-2.ac45694e.jpg"
+  },
+  {
+    "revision": "d001fcc5f0368268a1c75dd68239f8aa",
+    "url": "/static/media/slide-home-3.d001fcc5.jpg"
+  },
+  {
+    "revision": "b0b7e9492f667d48a277c94943882e7b",
+    "url": "/static/media/slide-home-4.b0b7e949.jpg"
+  },
+  {
+    "revision": "bd406683489a54f4d29b8667e8e5e7d4",
+    "url": "/static/media/slide-home-5.bd406683.jpg"
+  },
+  {
+    "revision": "bd96aa3c5a604969f466911dd2bb290b",
+    "url": "/static/media/slide-home-6.bd96aa3c.jpg"
+  },
+  {
+    "revision": "d24d14cc03819210008776820df2bf53",
+    "url": "/static/media/slide-home-7.d24d14cc.jpg"
+  },
+  {
+    "revision": "4ebe5f1834bd0972cc6a053899fd0726",
+    "url": "/static/media/slide-home-8.4ebe5f18.jpg"
+  },
+  {
+    "revision": "f9953c1e3d6283f64a74eac4f32207d1",
+    "url": "/static/media/slide-home-9.f9953c1e.jpg"
+  },
+  {
+    "revision": "93a3f8aa385dd0fc23992bdd8e7299ca",
+    "url": "/static/media/sochemlogo.93a3f8aa.png"
+  },
+  {
+    "revision": "cb8d93d355bc298b2d4480b4863f3074",
+    "url": "/static/media/test1.cb8d93d3.jpeg"
+  },
+  {
+    "revision": "eb4c4eaf2d258d4be987aa09df43b348",
+    "url": "/static/media/test2.eb4c4eaf.jpeg"
+  },
+  {
+    "revision": "f0b4ae72780171ed1389abe7a6427e14",
+    "url": "/static/media/test3.f0b4ae72.jpeg"
+  }
+]);
